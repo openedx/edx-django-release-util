40bf42b71380d4458ce9934ef5176fa4f08b888a
Troy Sankey <tsankey@edx.org>
2020-03-09 15:02:35 -0400

    fixup

--- setup.py
+++ setup.py
@@ -5,7 +5,34 @@ from setuptools import setup, find_packages
 
 import release_util
 
-long_description = io.open('README.rst', encoding='utf-8').read()
+
+def load_requirements(*requirements_paths):
+    """
+    Load all requirements from the specified requirements files.
+
+    Returns:
+        list: Requirements file relative path strings
+    """
+    requirements = set()
+    for path in requirements_paths:
+        requirements.update(
+            line.split('#')[0].strip() for line in open(path).readlines()
+            if is_requirement(line.strip())
+        )
+    return list(requirements)
+
+
+def is_requirement(line):
+    """
+    Return True if the requirement line is a package requirement.
+
+    Returns:
+        bool: True if the line is not blank, a comment, a URL, or an included file
+    """
+    return line and not line.startswith(('-r', '#', '-e', 'git+', '-c'))
+
+
+LONG_DESCRIPTION = io.open('README.rst', encoding='utf-8').read()
 
 setup(
     name='edx-django-release-util',
@@ -13,7 +40,7 @@ setup(
     description='edx-django-release-util',
     author='edX',
     author_email='oscm@edx.org',
-    long_description=long_description,
+    long_description=LONG_DESCRIPTION,
     license='AGPL 3.0',
     url='http://github.com/edx/edx-django-release-util',
     install_requires=load_requirements('requirements/base.in'),
